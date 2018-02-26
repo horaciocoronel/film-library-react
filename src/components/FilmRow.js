5b9ec0d5a8c05cd9418a10e5f744173a4029c9c8
horaciocoronel <arielc@gmail.com>
2018-02-26 15:07:53 -0500

    Component working with method that was refactored

--- src/components/FilmRow.js
+++ src/components/FilmRow.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 
 class FilmRow extends Component {
   render() {
-    return (
-			<div className="film-details">
-				<h1 className="section-title">DETAILS</h1>
-			</div>
-    );
+			const release_date = new Date(this.props.movie.release_date)
+			const posterUrl = `https://image.tmdb.org/t/p/w780/${this.props.movie.poster_path}`;
+			return (
+				<div className="film-row" key={this.props.movie.id}>
+				 <figure className="film-poster">
+					 <img src={posterUrl} alt={this.props.movie.title} />
+				 </figure>
+
+				 <div className="film-summary">
+					 <h1>{this.props.movie.title}</h1>
+					 <p>{release_date.getFullYear()}</p>
+				 </div>
+			 </div>
+		 	)
   }
 }
 
