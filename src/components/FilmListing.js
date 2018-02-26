350a8adafc9a2ed24360410672c5130eff5bea02
horaciocoronel <arielc@gmail.com>
2018-02-26 15:06:21 -0500

    Remove old method and refactor to use new FilmRow component create

--- src/components/FilmListing.js
+++ src/components/FilmListing.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
-
+import FilmRow from './FilmRow';
 class FilmListing extends Component {
-
-	renderMovie(movie) {
-		const release_date = new Date(movie.release_date)
-		const posterUrl = `https://image.tmdb.org/t/p/w780/${movie.poster_path}`;
-		return (
-			<div className="film-row" key={movie.id}>
-			 <figure className="film-poster">
-				 <img src={posterUrl} alt={movie.title} />
-			 </figure>
-
-			 <div className="film-summary">
-				 <h1>{movie.title}</h1>
-				 <p>{release_date.getFullYear()}</p>
-			 </div>
-		 </div>
-	 	)
-	}
 
   render() {
     return (
 				 <div className="film-list">
 					 <h1 className="section-title">FILMS</h1>
-					 {this.props.films.map(movie =>  this.renderMovie(movie)
+					 {this.props.films.map(movie =>  <FilmRow movie={movie} />
 				 )}
 				 </div>
     );
