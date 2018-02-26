ff2c1c996815ce7fe13dc26fed60be5254d278ba
horaciocoronel <arielc@gmail.com>
2018-02-26 14:21:28 -0500

    Add gitignore, to ignore cached files

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import FilmListing from './components/FilmListing.js';
 import FilmDetails from './components/FilmDetails.js';
 import logo from './logo.svg';
 import './App.css';
+import TMDB from './TMDB.js'
 
 class App extends Component {
   render() {
     return (
 			<div className="film-library">
-				 <FilmDetails />
-				 <FilmListing />
+				 <FilmDetails films={TMDB.films} />
+				 <FilmListing films={TMDB.films} />
 			 </div>
     );
   }
