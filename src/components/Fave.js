d3e5d0c8d984b1fc99cd1ef44f0e32ebe5e611f3
horaciocoronel <arielc@gmail.com>
2018-02-28 13:43:24 -0500

    Remove isFave state and replace onClickHandler function to call a function will pass later in the props

--- src/components/Fave.js
+++ src/components/Fave.js
@@ -4,19 +4,16 @@ class Fave extends Component {
 	constructor() {
 		super();
 		this.handleClick = this.handleClick.bind(this);
-		this.state = {
-			isFave: false
-		}
 	}
 
 	handleClick = (e) => {
 		e.stopPropagation();
-		this.setState({isFave: !this.state.isFave});
+		this.props.onFaveToggle();
 
 	};
 
   render() {
-		const queue = this.state.isFave ? 'remove_from_queue' : 'add_to_queue'
+		const queue = false ? 'remove_from_queue' : 'add_to_queue'
 		return (
 			<div className={`film-row-fave ${queue}`}>
 			  <i className="material-icons" onClick={this.handleClick}>add_to_queue</i>
