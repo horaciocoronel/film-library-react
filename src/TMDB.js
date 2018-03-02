56b68aaf9d6cb9dfd57cd628bf6d5d7addf9b0ab
horaciocoronel <arielc@gmail.com>
2018-03-02 01:20:23 -0500

    Move dotenv call to index.js

--- src/TMDB.js
+++ src/TMDB.js
@@ -1,7 +1,3 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 const TMDB = {
   api_key: process.env.REACT_APP_TMDB_API_KEY,
   films: [
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './normalize.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import dotenv from 'dotenv';
+dotenv.config();
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
